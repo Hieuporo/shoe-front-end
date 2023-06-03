d0842f85e3e6b7e2ed37ee4be170cd6257af3d5c
hieuduong <hieuark2003@gmai.com>
2023-06-03 17:19:01 +0700

    update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import PriceRange from "../components/PriceRange";
 import ProductItem from "../components/ProductItem";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const [selectedOption, setSelectedOption] = useState("");
@@ -139,56 +140,29 @@ const Products = () => {
 
             <div className="deal-leftmk left-side mt-4">
               <h3 className="agileits-sear-head">Special Deals</h3>
-              <div className="special-sec1">
-                <div className="col-xs-4 img-deals">
-                  <img src="./src/images/s4.jpg" alt="" />
-                </div>
-                <div className="col-xs-8 img-deal1">
-                  <h3>Shuberry Heels</h3>
-                  <a href="single.html">$180.00</a>
-                </div>
-                <div className="clearfix" />
-              </div>
-              <div className="special-sec1">
-                <div className="col-xs-4 img-deals">
-                  <img src="./src/images/s2.jpg" alt="" />
-                </div>
-                <div className="col-xs-8 img-deal1">
-                  <h3>Chikku Loafers</h3>
-                  <a href="single.html">$99.00</a>
-                </div>
-                <div className="clearfix" />
-              </div>
-              <div className="special-sec1">
-                <div className="col-xs-4 img-deals">
-                  <img src="./src/images/s1.jpg" alt="" />
-                </div>
-                <div className="col-xs-8 img-deal1">
-                  <h3>Bella Toes</h3>
-                  <a href="single.html">$165.00</a>
-                </div>
-                <div className="clearfix" />
-              </div>
-              <div className="special-sec1">
-                <div className="col-xs-4 img-deals">
-                  <img src="./src/images/s5.jpg" alt="" />
-                </div>
-                <div className="col-xs-8 img-deal1">
-                  <h3>Red Bellies</h3>
-                  <a href="single.html">$225.00</a>
-                </div>
-                <div className="clearfix" />
-              </div>
-              <div className="special-sec1">
-                <div className="col-xs-4 img-deals">
-                  <img src="./src/images/s3.jpg" alt="" />
-                </div>
-                <div className="col-xs-8 img-deal1">
-                  <h3>(SRV) Sneakers</h3>
-                  <a href="single.html">$169.00</a>
-                </div>
-                <div className="clearfix" />
-              </div>
+              {products &&
+                products.map((product, i) => {
+                  if (i < 4) {
+                    return (
+                      <Link
+                        className="special-sec1"
+                        to={`/product/${product.id}`}
+                        key={i}
+                      >
+                        <div className="col-xs-4 img-deals">
+                          <img src={product.image} alt="" />
+                        </div>
+                        <div className="col-xs-8 img-deal1">
+                          <h3>{product.name}</h3>
+                          <a>
+                            ${(product.price * (100 - product.discount)) / 100}
+                          </a>
+                        </div>
+                        <div className="clearfix" />
+                      </Link>
+                    );
+                  }
+                })}
             </div>
             {/* //deals */}
           </div>
